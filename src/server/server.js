20b3d46f87e9abc67633fc2777bb3e3550080793
tahir739 <mtahir_hussain@hotmail.com>
2019-12-04 19:08:08 +0100

    work in progress

--- src/server/server.js
+++ src/server/server.js
@@ -60,17 +60,80 @@ app.use(now);
   This endpoint returns the actual date
 */
 
-const players = router.get('/api/players2',
+const players_OLD = router.get('/api/players_OLD',
     (ctx) => {
         console.log('path: ', ctx.path);
         console.log('query: ', ctx.query);
         ctx.status = HttpStatus.OK;
         const Url = "https://api.airtable.com/v0/appir4fX2DVduzuQT/players?api_key=keyRn88PFIqnmOc0s";
+
         var json = request(Url);
         ctx.set('Content-Type', 'application/json');
+
+        /*
+        console.log('json ------->', json);
+        var playersList = [];
+        var records = json.Records;
+
+        records.forEach(function (record) {
+            var aPlayer;
+            aPlayer.name = record.fields.Name;
+
+            aPlayer.position = record.fields.Position;
+            aPlayer.attending = false;
+
+            playersList.push(aPlayer);
+        });*/
+        //ctx.body = playersList;
+        console.log('json ------->', json);
+
         ctx.body = json;
     })
+app.use(players_OLD);
+
+
+
+const players = router.get('/api/players', async (ctx) => {
+    try {
+        const record = await getBase();
+        console.log('Retrieved', record.id);
+        ctx.body = "Record ID from API: " + record.id;
+    } catch (err) {
+    }
+});
 app.use(players);
+
+
+function getBase() {
+    return new Promise((resolve, reject) => {
+        base('appir4fX2DVduzuQT').find('keyRn88PFIqnmOc0s', function (err, record) {
+            console.log('Retrieved', record.id);
+            if (err) {
+                reject(err)
+            } else {
+                resolve(record);
+            }
+        });
+    });
+}
+
+async function getPlayers() {
+    var Airtable = require('airtable');
+    var base = new Airtable({ apiKey: 'keyRn88PFIqnmOc0s' }).base('appir4fX2DVduzuQT');
+
+    var recordsList = [];
+    base('players').select({
+        view: 'Grid view'
+    }).firstPage(function (err, records) {
+        if (err) { console.error(err); return; }
+        records.forEach(function (record) {
+            recordsList.push(record);
+            console.log('Retrieved', record.get('Name'));
+        });
+    });
+    return recordsList;
+}
+
 
 /*
  This is where we start the server 
