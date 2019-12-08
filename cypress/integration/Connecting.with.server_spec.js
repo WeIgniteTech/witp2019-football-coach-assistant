f4f4d94e1583304bf7a02e9b3207a260fcc527eb
BoushraKolko <bushrakolko@hotmail.com>
2019-12-08 23:08:08 +0100

    a cypress test added

---
+++ cypress/integration/Connecting.with.server_spec.js
@@ -0,0 +1,8 @@
+describe('Connecting to server test', function() {
+    it('Does the application connect to server!', function() {
+      cy.visit('http://localhost:3004/');
+      cy.get('.modal').click();
+      cy.get('.btn-group > :nth-child(1)').click();
+      cy.get(':nth-child(2) > span').should(($txt)=>{expect($txt.text()).to.eq("Chosen Team size 3, is more than attending Players 0")})
+    })
+  })
